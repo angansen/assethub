eb85ca5cfa2c7fb8cc8b859508b6450eace120c6
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2019-11-19 11:13:15 +0000

    dev code sync

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,3 +1,7 @@
+git add /u01/ahweb/*
+git reset /u01/ahweb/backend/node_modules/*
+git commit -m "dev code sync"
+git push
 pm2 stop assethubbackend
 pm2 start /u01/ahweb/backend/bin/assethubbackend --watch
 echo "node backend restarted . . ."
