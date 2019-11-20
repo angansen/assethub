4e99eca11937c091735488d11d6f15e4c8d60003
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2019-11-20 14:30:34 +0000

    dev code sync

--- startup/gitsubmit.sh
+++ startup/gitsubmit.sh
@@ -2,5 +2,5 @@ git add /u01/ahweb/*
 git reset /u01/ahweb/backend/node_modules/*
 #git reset /u01/ahweb/startup/*
 git commit -m "dev code sync"
-git push --force
+git push https://angansen@gmail.com:Humanet@13github.com/angansen/assethub.git --force
 
