bfe41d05afac7a0ae50d56e4791594d2e4ef9d46
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2019-11-23 14:01:08 +0000

    dev code sync

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,4 +1,10 @@
-sh /u01/ahweb/gitsync.sh
+echo Do you wish to sync with git repository? Press 'y' or 'n'
+read issync
+if [ $issync == "y" ]; then
+        echo "syncing with repository"
+	sh /u01/ahweb/gitsync.sh
+fi
+
 pm2 stop assethubbackend
 pm2 start /u01/ahweb/backend/bin/assethubbackend --watch
 echo "node backend restarted . . ."
