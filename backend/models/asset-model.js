91f25751b654055107e79bd2bd905f78bbd2e899
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2019-11-27 14:18:08 +0000

    dev code sync

--- backend/models/asset-model.js
+++ backend/models/asset-model.js
@@ -2044,7 +2044,7 @@ module.exports = class Asset {
                     b.filter_name
                     from asset_search_activity a left join asset_filter b 
                     on a.activity_filter=b.filter_id 
-                    where a.activity_performed_by='`+ user_email + ` '
+                    where a.activity_performed_by='`+ user_email + `'  
                     and a.activity_filter is not null 
                     group by a.activity_filter,a.activity_type,b.filter_name 
                     order by count(*) desc,a.activity_type`, [],
@@ -2053,6 +2053,7 @@ module.exports = class Asset {
                         })
                         .then(result => {
                             countArr = result;
+                            console.log(JSON.stringify(countArr));
                             result.forEach(filter => {
                                 sugestionsarr.push(filter);
                             })
