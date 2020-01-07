0782769b2c3b6f90756807f1f3c457adb0840d0a
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2020-01-07 13:01:40 +0000

    dev code sync

--- backend/models/winstory/winstory-writer-model.js
+++ backend/models/winstory/winstory-writer-model.js
@@ -670,10 +670,10 @@ exports.SEAssistance = (request, res) => {
         WINSTORY_ID,
         USER_EMAIL,
         REQUEST_CREATED_ON,
-        REQUEST_DEMO_DATE,
+        SE_ASSISTANCE_DATE,
         REQUEST_OPPORTUNITY_ID,
-        REQUEST_DEMO_NOTE,
-        REQUEST_DEMO_CUSTOMER_NAME) values(:0,:1,:2,:3,:4,:5,:6,:7,:8,:9,:10)`;
+        SE_ASSISTANCE_NOTE,
+        SE_ASSISTANCE_CUSTOMER_NAME) values(:0,:1,:2,:3,:4,:5,:6,:7,:8,:9,:10)`;
     let saveDemoRequestOptions = [
         request.name,
         request.mobile,
@@ -682,10 +682,10 @@ exports.SEAssistance = (request, res) => {
         request.winstoryid,
         request.email,
         new Date(),
-        request.request_demo_date,
+        request.se_assistance_date,
         request.request_opportunity_id,
-        request.request_demo_note,
-        request.request_demo_customer_name];
+        request.se_assistance_note,
+        request.se_assistance_customer_name];
 
     connection.execute(saveDemoRequestSql, saveDemoRequestOptions, {
         autoCommit: true
