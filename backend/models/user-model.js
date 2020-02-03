88fc03890c2adbdaa19b00a11c458d68f070d7b1
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2020-02-03 21:32:09 +0000

    dev code sync

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -623,9 +623,9 @@ const triggerDeviceNotification = (content) => {
         console.log("+++++++++++++++ Device tokens ++++++++++++++++++")
         console.log(JSON.stringify(tokens));
         tokens.filter(token=>{
-            console.log(JSON.token);
-            console.log(token);
-            if(token.device_type=="iOS"){
+            console.log(JSON.stringify(token));
+            
+            if(token.device_type.toLowerCase().includes("ios")){
                 iosDevices.push(token.DEVICE_TOKEN);
             }else{
                 androidDevices.push(token.DEVICE_TOKEN);
