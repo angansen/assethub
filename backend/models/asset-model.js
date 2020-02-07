e5051583fbdaff888669ea396e4055d6fe541e6d
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2020-02-07 13:22:21 +0000

    dev code sync

--- backend/models/asset-model.js
+++ backend/models/asset-model.js
@@ -2337,7 +2337,7 @@ module.exports = class Asset {
                 .then(res => {
                     assetsArray = res
                     console.log('getFavAssets Length: ' + res.length)
-                    this.refineAssets(host, 0, -1, assetsArray, "", "", "", user_email).then(assets => {
+                    this.refineAssets(host, 0, assetsArray.length, assetsArray, "", "", "", user_email).then(assets => {
                         resolve(assets);
                     })
                 })
