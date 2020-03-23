e2b6f839d51829753218d3d51cbc27b94a6c9fd3
Oracle Public Cloud User <opc@ah-web-dev-02.sb4.ah.oraclevcn.com>
2020-03-23 16:31:56 +0000

    dev code sync

--- html/service-worker.js
+++ html/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0043d55e5f9c26bb740ca551a8e8ab83.js"
+  "/precache-manifest.a8285fcab97306714cdd81a7d490654b.js"
 );
 
 workbox.clientsClaim();
