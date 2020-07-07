c186c9b0635b5b41e594e09442d47eb2dbb276eb
Oracle Public Cloud User <opc@ah-web-02.sb2.ahvcn.oraclevcn.com>
2020-07-07 12:55:07 +0000

    dev code sync

---
+++ html/precache-manifest.1d0d8112559bfc1095c31df2392f33b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a85dcbcb3857962610d",
+    "url": "/static/css/main.656cb9fc.chunk.css"
+  },
+  {
+    "revision": "6a85dcbcb3857962610d",
+    "url": "/static/js/main.6a85dcbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "95a62213a1fce157da3f",
+    "url": "/static/css/2.fec0e736.chunk.css"
+  },
+  {
+    "revision": "95a62213a1fce157da3f",
+    "url": "/static/js/2.95a62213.chunk.js"
+  },
+  {
+    "revision": "bc35ab94176af9611d4e098f62f6d767",
+    "url": "/index.html"
+  }
+];
--- html/service-worker.js
+++ html/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec2c86ddcb46d50e60d89fcdd628aca5.js"
+  "/precache-manifest.1d0d8112559bfc1095c31df2392f33b7.js"
 );
 
 workbox.clientsClaim();
