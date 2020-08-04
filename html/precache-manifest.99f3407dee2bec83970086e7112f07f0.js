6466aa16010f35e46939fecd09a068a7add485e2
Oracle Public Cloud User <opc@ah-web-02.sb2.ahvcn.oraclevcn.com>
2020-08-04 16:58:09 +0000

    dev code sync

---
+++ html/precache-manifest.99f3407dee2bec83970086e7112f07f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7ebc665de6e9340fbe",
+    "url": "/static/css/main.c4aec8e4.chunk.css"
+  },
+  {
+    "revision": "8e7ebc665de6e9340fbe",
+    "url": "/static/js/main.8e7ebc66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "95a62213a1fce157da3f",
+    "url": "/static/css/2.fec0e736.chunk.css"
+  },
+  {
+    "revision": "95a62213a1fce157da3f",
+    "url": "/static/js/2.95a62213.chunk.js"
+  },
+  {
+    "revision": "17bb5178e4557afcdfd6551bc8fc5982",
+    "url": "/index.html"
+  }
+];
